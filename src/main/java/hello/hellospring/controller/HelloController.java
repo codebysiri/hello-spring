dda80083cf59cfa4af87d8f413ba3a9eb1f0eacc
S0I3R0I3 <71364866+S0I3R0I3@users.noreply.github.com>
2021-06-21 23:53:17 +0900

    Update Project (review)

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -13,6 +12,12 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 
 }
