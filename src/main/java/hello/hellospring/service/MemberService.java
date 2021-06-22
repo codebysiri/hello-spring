843faff7abd09e1503b1fbee3aa5f70a2b0f82f6
S0I3R0I3 <71364866+S0I3R0I3@users.noreply.github.com>
2021-06-23 00:40:40 +0900

    Update Project (review)

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,7 +9,11 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     // 회원가입
     public Long join(Member member) {
@@ -32,7 +36,7 @@ public class MemberService {
     }
 
     // 전체 회원 조회
-    private List<Member> findMembers() {
+    public List<Member> findMembers() {
         return memberRepository.findAll();
     }
 
